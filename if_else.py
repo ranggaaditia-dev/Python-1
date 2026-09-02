88f0b666048018ad8fb7fca0522fb2084766f8a3
rangga <ranggagithub@gmail.com>
2026-09-03 00:11:01 +0700

    IF ELSE

---
+++ if_else.py
@@ -0,0 +1,10 @@
+nama = input("Maasukan nama anda :")
+
+if nama == "Rangga":
+    print("Nama anda Rangga")
+elif nama == "budi":
+    print("Nama anda budi")
+elif nama == "siti":
+    print("Nama anda siti")
+else:
+    print("Nama anda salah")
