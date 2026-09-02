5e0f2bd858bb7bcf5fd69994393cf590d32dbd9d
rangga <ranggagithub@gmail.com>
2026-09-02 09:50:24 +0700

    PENJUALAN HARGA BUKU

---
+++ main.py
@@ -0,0 +1,34 @@
+print ("\nMENJUAL ALAT ALAT BELAJAR")
+
+BUKU = 25000
+while BUKU:
+    BUKU = int(input("\nMasukan harga anda :"))
+    if BUKU == 25000:
+        print("SALDO ANDA CUKUP BELI BUKU")
+    elif BUKU >= 15000:
+        print("SALDO ANDA HANYA CUKUP BELI PULPEN")
+    else:
+        print("SALDO ANDA TIDAK CUKUP")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
