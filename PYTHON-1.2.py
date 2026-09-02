20e22162e442411259f6f66ad01bfb5d41008284
rangga <ranggagithub@gmail.com>
2026-09-03 00:02:45 +0700

    PELAJARAN 1.2

---
+++ PYTHON-1.2.py
@@ -0,0 +1,23 @@
+print("BELAJAR VARIABEL")
+
+nama = "Rangga"
+kelas = 12
+umur = 19
+print("HALO PERKENALKAN NAMA SAYA", nama)
+print("SAYA KELAS", kelas)
+print(umur == 19)
+print(umur == 17)
+print(umur >= 19)
+
+
+print("")
+nama = "Rangga"
+kelas = 12
+umur = 19
+tinggi_badan = 1.40
+print(f"HALLO NAMA SAYA ADALAH {nama}, SAYA KELAS {kelas},DAN UMUR SAYA {umur}, TINGGI BADAN SAYA SEKITAR {tinggi_badan}")
+
+
+
+
+
