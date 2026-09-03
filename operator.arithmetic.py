f7b889ce9c7c58af0647cb89896835a42ec3fcff
rangga <ranggagithub@gmail.com>
2026-09-03 09:11:56 +0700

    OPERATOR

--- operator.arithmetic.py
+++ operator.arithmetic.py
@@ -1,3 +1,4 @@
+print("=======OPERATOR ARITHMETIC")
 umur = 17
 kelas = 12
 
@@ -8,3 +9,23 @@ is_genap = kelas % 2 == 0
 print(f"jumlah: {jumlah}")  
 print(f"selisih: {selisih}")
 print(f"is_genap: {is_genap}")
+
+
+
+print("=======OPERATOR COMPARISON")
+umur = 17
+kelas = 12
+
+print(umur == 17)
+print(kelas != 10)
+print(umur >= 18)
+print(kelas < 12)
+
+
+
+print("=======OPERATOR COMPARISON")
+
+
+
+
+
