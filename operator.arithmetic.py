507decf506620769e59408fca1bfd8809cd6629a
rangga <ranggagithub@gmail.com>
2026-09-03 08:49:23 +0700

    main

---
+++ operator.arithmetic.py
@@ -0,0 +1,10 @@
+umur = 17
+kelas = 12
+
+jumlah = umur + kelas 
+selisih = umur - kelas
+is_genap = kelas % 2 == 0
+
+print(f"jumlah: {jumlah}")  
+print(f"selisih: {selisih}")
+print(f"is_genap: {is_genap}")
